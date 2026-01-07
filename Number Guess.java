e519bd2a0e75dfb4975c3af812681668922f3c4b
UMAL WARA <umalwaraarain123786@gmail.com>
2026-01-07 19:03:13 +0500

    Add number guessing game in Number Guess.java
    Implemented a number guessing game with user input and feedback on guesses.

---
+++ Number Guess.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Games {
+public static void main(String[] args){
+// Number guessing game
+Scanner sc = new Scanner(System.in);
+int myNumber = (int)(Math.random() * 100); // 0–99
+int userNumber;
+int attempts = 0;
+ do {
+ System.out.print("Guess my number: ");
+ userNumber = sc.nextInt();
+ //Counting number of attempts
+ attempts++;
+
+ if (userNumber == myNumber) {
+ System.out.println("WOOHOO! Correct number!!!");
+ break;
+    }
+ else if (userNumber > myNumber) {
+ System.out.println("Your number is too large");
+    }
+ else {
+ System.out.println("Your number is too small");
+    }
+    } while (userNumber >= 0);
+
+  System.out.println("My number was: " + myNumber);
+  if (userNumber == myNumber) {
+  System.out.println("You made the correct guess in " + attempts + " tries!");
+  if (attempts < 5)
+  System.out.println("Great job!!!");
+  else if (attempts < 10)
+  System.out.println("Best!!");
+  else
+  System.out.println("Good!");
+         }
+            }
+        }
