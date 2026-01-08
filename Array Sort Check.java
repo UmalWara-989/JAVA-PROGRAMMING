1d133959d582a6f2629084bee971add82eac7de3
UMAL WARA <umalwaraarain123786@gmail.com>
2026-01-08 17:13:14 +0500

    Add array sorting check functionality

---
+++ Array Sort Check.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Arrays {
+    public static void main(String[] args) {
+// Check Sorting of an Array
+Scanner sc = new Scanner(System.in);     
+System.out.print("Provide Array size : ");
+int s = sc.nextInt();
+
+//Declare 1-D Array
+int[] sort = new int[s];
+      
+//Input
+for (int i = 0; i < sort.length; i++) {
+System.out.print("Enter value " + (i + 1) + " = ");
+//Take integer array as input
+sort[i] = sc.nextInt();
+      }
+      
+boolean ascending = true;
+boolean descending = true;
+for (int i = 0; i < sort.length - 1; i++) {
+if (sort[i] > sort[i + 1]) {
+      ascending = false;
+  }
+if (sort[i] < sort[i + 1]) {
+        descending = false;
+}
+    }
+if (ascending) {
+System.out.println("Sorted in Ascending order");}
+  else if (descending) {
+  System.out.println("Sorted in Descending order");}
+    else {
+       System.out.println("Neither in Ascending nor in Descending order");
+        }
+    }
+}
